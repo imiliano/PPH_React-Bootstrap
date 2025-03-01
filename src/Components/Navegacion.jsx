bdd33cd3fc4509ae253eaa4c6c8f45e51e74deb9
max <maximiliano.g.rubino@gmail.com>
2025-03-01 22:12:18 +0100

    Update Navegacion.jsx

--- src/Components/Navegacion.jsx
+++ src/Components/Navegacion.jsx
@@ -6,49 +6,44 @@ export const Navegacion = () => {
     <div className="center-class">
     <NavLink
     className="text-decoration-none m-2" to={ '/' } >
-        <img className="img-fluid" width={180} src="/icon.png" alt="icon"/>
+        <img className="img-fluid" width={170} src="/icon.png" alt="icon"/>
     </NavLink>
 
 <Navbar className="center-class mt-2" collapseOnSelect expand='md'>
-            <NavbarToggle className="mb-3" />
-            <Navbar.Collapse>
+        <NavbarToggle className="mb-3" />
+    <Navbar.Collapse>
                 
-                <Nav className="ubuntu-bold text-center d-flex flex-md-row justify-content-md-between">
-
+<Nav className="ubuntu-bold text-center d-flex flex-md-row justify-content-md-between">
 
     <NavLink
     className={ ({ isActive }) => `text-uppercase text-decoration-none fs-5 mx-2 ${ isActive ? 'active' : ''}` }
     to={ 'galeria' }
-    >
-        Galeria
+    > Galeria
     </NavLink>
 
     <NavLink
     to={ 'contacto'} 
     className="text-uppercase text-decoration-none fs-5 mx-2"
-    >
-        Contacto
+    > Contacto
+    </NavLink>
+
+    <NavLink
+    to={ 'carnet' }
+    className="text-uppercase text-decoration-none fs-5 mx-2"
+    > Carnet Alberguista
     </NavLink>
 
     <NavLink 
     target="blank_"
     className="text-uppercase text-decoration-none fs-5 mx-2"
     to={`https://booking.avirato.com/?code=6818963`} 
-    >
-        Reserva
+    > Reserva
     </NavLink>
+</Nav>
 
-    <NavLink
-    to={ 'carnet' }
-    className="text-uppercase text-decoration-none fs-5 mx-2"
-    >
-        Carnet Alberguista
-    </NavLink>
-                </Nav>
+    </Navbar.Collapse>
 
-            </Navbar.Collapse>
-
-        </Navbar>
+</Navbar>
     </div>
   )
 }
