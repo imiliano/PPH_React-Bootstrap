052c4cee1c5610cfe25cbda6158b4267eba5025f
max <maximiliano.g.rubino@gmail.com>
2025-03-01 19:36:41 +0100

    Update Inicio.jsx
    <p /> X <section />

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -10,30 +10,30 @@ export const Inicio = () => {
 <div className="col-md-3">
     <img className="rounded img-fluid" src="/Zona_Comun.webp" alt="imagen salon-comedor" />
         <h1 className="text-secondary">salón, comedor</h1>
-            <p className="text-uppercase text-primary ubuntu-bold">
+            <section className="mb-1 text-uppercase text-primary ubuntu-bold">
                 <li>televisión</li> 
                 <li>máquina de café</li>  
                 <li>máquina vending </li> 
                 <li>juegos de mesa</li>
-            </p>
+            </section>
 </div>
 
 <div className="col-md-3">
     <img className="rounded img-fluid" src="/Habitaciones.webp" alt="imagen habitacion" />
         <h1 className="text-secondary">habitaciones</h1>
-            <p className="text-uppercase text-primary ubuntu-bold">
+            <section className="mb-1 text-uppercase text-primary ubuntu-bold">
                 <li>10, 8, 6 y 4 camas por habitación</li>
                 <li>taquillas individuales</li>
                 <li>luz, enchufe y balda</li>
-                <li>aire acondicionado y calefacción</li>
+                <li>aire acondicionado</li>
                 <li>calefacción</li>
-            </p>
+            </section>
 </div>
 
 <div className="col-md-3">
     <img className="rounded img-fluid" src="/cocina.webp" alt="imagen habitacion" />
         <h1 className="text-secondary">cocina</h1>
-            <p className="text-uppercase text-primary ubuntu-bold">
+            <section className="mb-1 text-uppercase text-primary ubuntu-bold">
                 <li>frigorífico</li>
                 <li>congelador</li>
                 <li>vitrocerámica</li>
@@ -41,21 +41,21 @@ export const Inicio = () => {
                 <li>menaje de cocina</li>
                 <li>lavadora</li>
                 <li>secadora</li>
-            </p>
+            </section>
 </div>
 
 <div className="col-md-3">
     <img className="rounded img-fluid" src="/terraza.webp" alt="imagen terraza" />
         <h1 className="text-secondary">servicios adicionales</h1>
-            <p className="text-uppercase text-primary ubuntu-bold">
+            <section className="mb-1 text-uppercase text-primary ubuntu-bold">
                 <li>terraza</li>
                 <li>wifi gratuito</li>
                 <li>parking de bicicletas</li>
                 <li>cámaras de seguridad</li>
                 <li><a className="text-decoration-none" href="https://www.palmaportlockers.com">servicio de consignas</a> (de pago)</li>
-            </p>
+            </section>
 </div>
-
+      
     </div>
 </div>
 
