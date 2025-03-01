658b31c9545a19fcf170fb4140c97ce668fe333e
max <maximiliano.g.rubino@gmail.com>
2025-03-01 11:48:40 +0100

    Adding main files to the project.

---
+++ HostelApp.jsx
@@ -0,0 +1,17 @@
+
+import { Footer } from "./src/Components/Footer"
+import { Header } from "./src/Components/Header"
+import { AppRouter } from "./src/Router/AppRouter"
+
+
+export const HostelApp = () => {
+  return (
+<>
+    <Header />
+
+    <AppRouter />
+    
+    <Footer />
+</>
+  )
+}
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+
+export const Footer = () => {
+
+  return (
+
+    <footer className="center-class ubuntu-bold mt-3">
+        <p>&copy; Palma Port Hostel </p>
+        <span>{ new Date().getFullYear() }</span>
+    </footer>
+
+  )
+}
