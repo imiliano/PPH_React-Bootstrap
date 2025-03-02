55e8e3af803565b4ad306b7ccf78b6e0960fce32
max <maximiliano.g.rubino@gmail.com>
2025-03-02 22:50:49 +0100

    Update main.jsx
    Añadiendo código de i18next e importando i18next provider.
    Importando global.json para las traducciones.
    Cambiando BrowserRouter X HashRouter para solucionar error 404 al refrescar la página. 
    * HashRouter agrega un ' # '  por delante del nombre de la ruta evitando solicitudes directas al servidor.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import { HostelApp } from '../HostelApp'
-import { BrowserRouter } from 'react-router-dom'
+
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+import global_es from './translations/es/global.json';
+import global_en from './translations/en/global.json';
+import global_ge from './translations/ge/global.json';
+import global_fr from './translations/fr/global.json';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources:{
+    es: {
+      global: global_es
+    },
+    en: {
+      global: global_en
+    },
+    ge: {
+      global: global_ge
+    },
+    fr: {
+      global: global_fr
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+
+    <I18nextProvider i18n={ i18next }>
+
+    <HashRouter>
 
       <HostelApp />
       
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>
+    
+    </I18nextProvider>
+
+  </StrictMode>
 )
