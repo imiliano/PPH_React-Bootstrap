26923a0091b4dacc8d109a91e16f5c9372bdd841
max <maximiliano.g.rubino@gmail.com>
2025-03-02 02:00:49 +0100

    Update Footer.jsx
    Buttons for translations

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,40 @@
+import { Button } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+
 
 export const Footer = () => {
+  const [ t, i18n ] = useTranslation('global')
 
   return (
-
+<>
     <footer className="center-class ubuntu-bold mt-3">
-        <p>&copy; Palma Port Hostel </p>
-        <span>{ new Date().getFullYear() }</span>
-    </footer>
+        <p>&copy; <span>{ new Date().getFullYear() }</span> - Palma Port Hostel - {t("Footer.AlbergueJuvenil")}</p>
 
+<section>
+<div className="col-md-3 p-1">
+    <Button className="btn-sm" variant="outline-dark fw-bold" onClick={() => i18n.changeLanguage('es')}>
+    ES
+    </Button>
+</div>
+
+<div className="col-md-3 p-1">
+    <Button className="btn-sm" variant="outline-dark fw-bold" onClick={() => i18n.changeLanguage('en')}>
+    EN
+    </Button>
+</div>
+<div className="col-md-3 p-1">
+    <Button className="btn-sm" variant="outline-dark fw-bold" onClick={() => i18n.changeLanguage('ge')}>
+    GE
+    </Button>
+</div>
+<div className="col-md-3 p-1">
+    <Button className="btn-sm" variant="outline-dark fw-bold" onClick={() => i18n.changeLanguage('fr')}>
+    FR
+    </Button>
+</div>
+</section>
+
+    </footer>
+</>
   )
 }
