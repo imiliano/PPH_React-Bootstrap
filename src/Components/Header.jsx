f4689cd86c37967b598875cfac1095deb3b17bdd
max <maximiliano.g.rubino@gmail.com>
2025-03-02 23:19:40 +0100

    Update Header.jsx
    Agregando <Alert />

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,23 @@
-import { Navegacion } from "./Navegacion"
+import { Alert } from 'react-bootstrap';
+import { Navegacion } from './Navegacion';
+import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
 
 
 export const Header = () => {
+      const [ t ] = useTranslation('global');
+
   return (
+    <>
     
     <Navegacion />
 
+    <Alert className="ubuntu-bold fs-5 text-center" variant="info">
+      <NavLink className='text-decoration-none' to={ 'carnet' }>
+    {t("Carnet.mandatory")}
+      </NavLink>
+    </Alert>
+
+    </>
   )
 }
